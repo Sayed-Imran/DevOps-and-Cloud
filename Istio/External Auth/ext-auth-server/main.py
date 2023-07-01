f9aa38a1107ce86253e7ff0b0a359e834379e8b1
Sayed-Imran <sayedimran00786@gmail.com>
2023-07-01 17:01:55 +0530

    Added Docstring

--- Istio/External Auth/ext-auth-server/main.py
+++ Istio/External Auth/ext-auth-server/main.py
@@ -5,6 +5,16 @@ app = FastAPI()
 
 @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"])
 async def auth(path:str, role: str = Header(default=None)):
+    """
+    The auth function is a simple authorization function that checks the role of the user.
+    If the user has an admin role, it returns a 200 response with an admin header.
+    Otherwise, it returns a 401 response with an anonymous header and body message.
+
+    :param path:str: Get the path of the request
+    :param role: str: Pass the role of the user to the auth function
+    :return: A jsonresponse object
+    :doc-author: Sayed Imran
+    """
 
     try:
         print(f"Role: {role}")
