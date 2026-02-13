4c97e51ee30c53b3b254871b23bf596ebe84c09b
sayed-imran <sayedimran00786@gmail.com>
2026-02-13 20:36:47 +0000

    Refactor Istio cluster setup script to streamline installation and service exposure for east-west gateways

--- Istio/MultiCluster-Kind/istio-cluster-setup.sh
+++ Istio/MultiCluster-Kind/istio-cluster-setup.sh
@@ -1,27 +1,29 @@
 set -e 
+export CTX_CLUSTER1="kind-cluster1"
+export CTX_CLUSTER2="kind-cluster2"
+
+# Install Istio on cluster1
 istioctl install --context="${CTX_CLUSTER1}" -f cluster1.yaml -y
-cd /home/imran/istio-1.25.2
 
-samples/multicluster/gen-eastwest-gateway.sh \
-    --network network1 | \
-    istioctl --context="${CTX_CLUSTER1}" install -y -f -
+# Install east-west gateway on cluster1
+istioctl --context="${CTX_CLUSTER1}" install -y -f eastwest-gateway-cluster1.yaml
 
-kubectl --context="${CTX_CLUSTER1}" apply -n istio-system -f \
-    samples/multicluster/expose-services.yaml
+# Expose services on cluster1
+kubectl --context="${CTX_CLUSTER1}" apply -n istio-system -f expose-services.yaml
 
-kubectl get secrets --context="${CTX_CLUSTER1}"  -n istio-system \
-istio-ca-secret -oyaml > istio-ca-secrets.yaml
+# Export CA secrets from cluster1
+kubectl get secrets --context="${CTX_CLUSTER1}" -n istio-system \
+    istio-ca-secret -oyaml > istio-ca-secrets.yaml
 
-kubectl create ns istio-system --context $CTX_CLUSTER2
-kubectl apply -f istio-ca-secrets.yaml --context $CTX_CLUSTER2
+# Create istio-system namespace and apply CA secrets on cluster2
+kubectl create ns istio-system --context="${CTX_CLUSTER2}"
+kubectl apply -f istio-ca-secrets.yaml --context="${CTX_CLUSTER2}"
 
-cd /home/imran/projects/kubernetes/DevOps-and-Cloud/Istio/MultiCluster-Kind
-
+# Install Istio on cluster2
 istioctl install --context="${CTX_CLUSTER2}" -f cluster2.yaml -y
-cd /home/imran/istio-1.25.2
-samples/multicluster/gen-eastwest-gateway.sh \
-    --network network2 | \
-    istioctl --context="${CTX_CLUSTER2}" install -y -f -
 
-kubectl --context="${CTX_CLUSTER2}" apply -n istio-system -f \
-    samples/multicluster/expose-services.yaml
+# Install east-west gateway on cluster2
+istioctl --context="${CTX_CLUSTER2}" install -y -f eastwest-gateway-cluster2.yaml
+
+# Expose services on cluster2
+kubectl --context="${CTX_CLUSTER2}" apply -n istio-system -f expose-services.yaml
